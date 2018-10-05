061796d8d6ada719e0a134ac50661c5a39e4105a
sekobey83 <serkan.yildirim@aurea.com>
2018-10-05 06:08:30 +0300

    First commit

---
+++ src/test/java/com/PalindromeTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PalindromeTest {
+
+  @Test
+  public void whenEmptyString_thenAccept() {
+    Palindrome palindromeTester = new Palindrome();
+    assertTrue(palindromeTester.isPalindrome(""));
+  }
+}
